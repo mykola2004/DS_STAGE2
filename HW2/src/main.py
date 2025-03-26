12a6b16afa5b8e57ec6e0901c3dc2d1ffdb439e2
mykola2004 <144995714+mykola2004@users.noreply.github.com>
2025-03-26 16:01:55 +0100

    Add files via upload

---
+++ HW2/src/main.py
@@ -0,0 +1,12 @@
+import transformers
+import torch
+import pandas as pd
+import sklearn
+
+print("Hello!9999999999999999999")
+
+pathway = "../data/train.csv"
+
+df = pd.read_csv(pathway)
+
+print(df.head())
