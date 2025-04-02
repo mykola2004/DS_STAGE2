1b8a23173954c25fcc6aa88f39a2ca61aa61d501
mykola2004 <144995714+mykola2004@users.noreply.github.com>
2025-04-02 14:59:33 +0200

    updated main.py

--- HW2/src/main.py
+++ HW2/src/main.py
@@ -4,10 +4,11 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 from xgboost import XGBClassifier
 
+print("Loading data")
 filepath = "/app/data/iris.csv"
-
 df = pd.read_csv(filepath)
 
+print("Creating test and train sets")
 X = df.drop("species", axis=1)
 y = df["species"]
 
@@ -16,9 +17,11 @@ y_encoded = encoder.fit_transform(y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=1)
 
+print("Training XGBoost classifier")
 model = XGBClassifier()
 model.fit(X_train, y_train)
 
+print("Calculating accuracy")
 preds = model.predict(X_test)
 acc = accuracy_score(y_test, preds)
 
