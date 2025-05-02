05b8e1ecfe99850b39f86cef0fabc3ab07314f3b
Mykola Chernenko <mykolachernenko2004@gmail.com>
2025-05-02 17:40:33 +0200

    fixed cron, now it works inside of a container

--- HW5/batch_deployment/cronjob.sh
+++ HW5/batch_deployment/cronjob.sh
@@ -1,2 +1,1 @@
-*/1 * * * * /usr/local/bin/python /app/batch_deployment/pipeline.py >> /var/log/cron.log 2>&1
-#blank line
+*/1 * * * * root /usr/local/bin/python /app/batch_deployment/pipeline.py >> /var/log/cron.log 2>&1
